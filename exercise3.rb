80209a4596b5bc0d0397eb62063d75469497f4c7
Graham Moss <graham.moss@hotmail.com>
2017-01-17 14:27:53 -0500

    completed exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,7 @@
 puts "What is your name?"
+my_name = gets.chomp
+puts "Hi #{my_name}!"
+
+puts "#{my_name}, How old are you?"
+my_age = gets.chomp.to_i
+puts "Your year of birth is #{2017 - my_age}."
