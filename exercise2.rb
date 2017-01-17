2d3b8d11406447b842ed1ef17b271f1d9d5ce6c1
Graham Moss <graham.moss@hotmail.com>
2017-01-16 21:29:42 -0500

    completed exercise2

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+#How would you calculate a good tip for a 55 dollar meal?
+puts 55 * 0.15
+#Find a way to convert the integer into a string
+numba = (5 + 5)
+puts numba
+#Try outputting the result of 45628 multiplied by 7839 in a sentence
+puts "#{45628 * 7839} is a big number."
+#What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+#true
